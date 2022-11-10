105e6f932566ba35f2d9816e05ef51e16e69cdb6
Hernan Abel Diaz Fuentes <h.diaz@globant.com>
2022-11-10 11:06:05 -0300

    delete copyright

--- src/main/java/com/example/globantpersonalproject/application/mapper/ConverterMovie.java
+++ src/main/java/com/example/globantpersonalproject/application/mapper/ConverterMovie.java
@@ -13,18 +13,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-/*
- * Copyright 2021 MonetaGo, Inc. All Rights Reserved.
- *
- * This code is copyrighted material that is confidential and proprietary to MonetaGo, Inc.
- * and may not (in whole or in part) be published, publicly displayed, copied, modified or
- * used in any way other than as expressly permitted in a written agreement executed by
- * MonetaGo, Inc. No portion of this code may be used to create derivative works or exploited
- * in any other way without MonetaGo, Inc.'s prior written consent. No portion of this code
- * may be transmitted or redistributed to any person without MonetaGo, Inc.'s prior written
- * consent. This notice may not be deleted or modified without MonetaGo, Inc.'s consent.
- */
-
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class ConverterMovie implements Converter<Optional<Movie>, MovieDto> {
